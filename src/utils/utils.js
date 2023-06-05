eda7dbf18af1f183192002d0de6657c23002d20e
Maryna Danylchuk <marina123456mari@gmail.com>
2023-06-05 21:08:36 +0300

    change BaseUrl

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 const defaultOptions = {
-    baseURL: "https://danylchuk-m.herokuapp.com/api/",
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  };
+  baseURL: "http://localhost:3001/api/",
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
 
 let instance = axios.create(defaultOptions);
-  
+
 export default instance;
